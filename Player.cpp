d250c7d5367f9dcd5a401d289929d6d73273a4b8
NunezGabriel <emilio.nunez.arenas@gmail.com>
2021-11-08 17:57:21 -0500

    subiendo class player

---
+++ Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+Player::Player(int posX, int posY, int HP, int DEF, int ATK){
+	
+	x = posX;
+	y = posY;
+	hp = HP;
+	def = DEF;
+	atk = ATK;
+	exp = EXP;
+	
+}
+
---
+++ Player.h
@@ -0,0 +1,16 @@
+#include <Windows.h>
+
+class Player{
+	
+public:
+
+//constructor
+	Player(int posX, int posY, int HP, int DEF, int ATK, int EXP )
+//variables
+	int hp; 
+	int def;
+	int atk;	
+	int x;
+	int y;	
+	int exp;
+};
