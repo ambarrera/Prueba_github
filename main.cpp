aaada0b2619bf0217a942df500a5555ecd05fd6d
milksake <caceresmariana443@gmail.com>
2021-11-26 00:38:37 -0500

    Changed main.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Game.h"
 
 int main() {
-    Game game(120, 30, 5); //Instancia de la clase game de 120 x 30
+    Game game(120, 30, 6); //Instancia de la clase game de 120 x 30
     game.run();
     return 0;
 }
